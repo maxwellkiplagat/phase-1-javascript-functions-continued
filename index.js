7302af7eb1ce490dfff40c205bac5a7da8a54eb2
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-03-04 12:12:58 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity= 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+    
+}
+saturdayFun();
+function mondayWork(activity= 'go to the office') {
+    return `This Monday, I will ${activity}.`    
+}
+
+wrapAdjective('*');
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  const wrapWithStar = wrapAdjective("*");
+console.log(wrapWithStar("awesome")); // "*awesome*"
+
+const wrapWithPipes = wrapAdjective("||");
+console.log(wrapWithPipes("amazing")); // "||amazing||"
+
+console.log(wrapAdjective()("cool")); // "*cool*" (default wrapper)
